0e3083d33de517bbdf3376ff065d93e022ef5d72
Thomas Harris <ta.harris.42@gmail.com>
2023-03-19 14:47:01 +0100

    add graceful shutdown, for kicks

--- cmd/mlb-rss/main.go
+++ cmd/mlb-rss/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/0queue/mlb-rss/internal/cache"
@@ -14,9 +17,9 @@ import (
 )
 
 type config struct {
-	Json bool   `envDefault:"false"`
-	Addr string `envDefault:":8080"`
-	Cron string `envDefault:"0 7 * * *"`
+	JsonLog bool   `envDefault:"false"`
+	Addr    string `envDefault:":8080"`
+	Cron    string `envDefault:"0 7 * * *"`
 }
 
 func main() {
@@ -26,12 +29,12 @@ func main() {
 		UseFieldNameByDefault: true,
 	}
 	if err := env.Parse(&c, opts); err != nil {
-		fmt.Printf("Failed to parse config: %w", err)
+		fmt.Printf("Failed to parse config: %s", err)
 		os.Exit(1)
 	}
 
 	var handler slog.Handler
-	if c.Json {
+	if c.JsonLog {
 		handler = slog.NewJSONHandler(os.Stdout)
 	} else {
 		handler = slog.NewTextHandler(os.Stdout)
@@ -42,20 +45,48 @@ func main() {
 	cache := cache.Cache[report2.Report2]{}
 	cache.Set(report2.GenerateReport())
 
+	// prepare shutdown channel
+	// this signalCtx goes to the report generator
+	// not the http server though, because it is already cancelled
+	signalCtx, signalCancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer signalCancel()
+
 	// TODO figure out implications of local time
 	// start refresh cron job
-	s := gocron.NewScheduler(time.Local)
-	s.Cron(c.Cron).Do(func() {
+	cron := gocron.NewScheduler(time.Local)
+	cron.Cron(c.Cron).Do(func() {
 		fmt.Println("Doing the thing")
 	})
 
-	s.StartAsync()
+	cron.StartAsync()
 
 	// serve xml
-	http.HandleFunc("/rss.xml", func(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/rss.xml", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
 
+	server := http.Server{
+		Addr:    c.Addr,
+		Handler: mux,
+	}
+
 	slog.Info("Starting http server", slog.String("addr", c.Addr))
-	http.ListenAndServe(c.Addr, nil)
+
+	go func() {
+		server.ListenAndServe()
+	}()
+
+	slog.Info("mlb-rss ready")
+	<-signalCtx.Done()
+
+	slog.Info("Shutting down")
+
+	timeout, timeoutCancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer timeoutCancel()
+
+	cron.Stop()
+	server.Shutdown(timeout)
+
+	slog.Info("Shutdown finished")
 }
